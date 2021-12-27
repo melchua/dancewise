03c71685c772b4029378cfe8410037161d2b27f7
Mel Chua <mchuam@gmail.com>
2021-12-27 13:42:56 -0800

    Clean up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,10 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def index
+    @users = User.all
+  end
   
   def create
     @user = User.new(user_params)
@@ -27,7 +31,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'edit'
     end
