aeee7f1c98b20ae7e748c520246361eb1ee82b2f
Mel Chua <mchuam@gmail.com>
2021-12-30 20:18:02 -0800

    Add dance styles and styling

---
+++ db/migrate/20211231020507_remove_dance_type_from_event.rb
@@ -0,0 +1,5 @@
+class RemoveDanceTypeFromEvent < ActiveRecord::Migration[6.1]
+  def up
+    remove_column :events, :dance_type
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_31_012750) do
+ActiveRecord::Schema.define(version: 2021_12_31_020507) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 2021_12_31_012750) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
-    t.string "dance_type"
     t.string "description"
     t.string "image_url"
     t.integer "user_id"
