bc686a352ebdca55bb4ca61a634c7efb982768f7
ogarocious <awesomeogar@gmail.com>
2022-04-30 01:44:39 -0700

    Adding Geocoder Gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_07_102337) do
+ActiveRecord::Schema.define(version: 2022_04_29_223615) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.integer "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -78,6 +106,9 @@ ActiveRecord::Schema.define(version: 2022_03_07_102337) do
     t.datetime "event_date"
     t.integer "event_type_id", default: 1
     t.integer "event_frequency_id", null: false
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["event_frequency_id"], name: "index_events_on_event_frequency_id"
     t.index ["event_type_id"], name: "index_events_on_event_type_id"
   end
@@ -91,6 +122,8 @@ ActiveRecord::Schema.define(version: 2022_03_07_102337) do
     t.boolean "admin", default: false
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "events", "event_frequencies"
   add_foreign_key "events", "event_types"
 end
